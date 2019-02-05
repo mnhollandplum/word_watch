5dd19ca93ba82b6fac02bbfc626ab09b793f834a
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-02-05 09:37:39 -0700

    Top word and count display on page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import $ from 'jquery'
 $(document).ready(() => {
   // have fun!
 
+//get data from api
   fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+//turn it in to json
   .then(response => response.json())
-  .then(top_word => console.log(top_word))
+//pass json to display variable
+  .then(top_word => displayWord(top_word))
+//error out
   .catch((error) => console.error({ error }))
 
-
+//create display variable, pass in json
+  const displayWord = (top_word) => {
+//get the word
+    const word = Object.keys(top_word.word)[0];
+// get the count of how many times it was searched
+    const wordCount = Object.values(top_word.word)[0];
+//change the html header to add in the word and count to the end
+    $('.top-word h3').append(`"${word}" = ${wordCount} times`);
+  }
 })
