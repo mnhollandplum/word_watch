7bd8c9ddb67217e4ffcd565630dc0ec7c5dfc3d1
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-02-05 09:16:35 -0700

    Pull in api data for top word

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import $ from 'jquery'
 
 $(document).ready(() => {
   // have fun!
+
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+  .then(response => response.json())
+  .then(top_word => console.log(top_word))
+  .catch((error) => console.error({ error }))
+
+
 })
